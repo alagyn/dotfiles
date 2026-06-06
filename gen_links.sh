d1b05bcd1e58e4811ac982c679ac42a413751031
alagyn <benkimbrough44@gmail.com>
2026-06-06 12:22:39 -0400

    update helix

--- gen_links.sh
+++ gen_links.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 
-SCRIPT_ROOT=$(realpath $(dirname $0))
+SCRIPT_ROOT=$(realpath $(dirname "$0"))
 
 # VS CODE configs
 VSCODE_CFG_DIR=~/.config/Code/User
-mkdir --parents $VS_CFG_DIR
+mkdir --parents "$VS_CFG_DIR"
 rm -f $VSCODE_CFG_DIR/settings.json
-ln -s $SCRIPT_ROOT/VSCode/settings.json $VSCODE_CFG_DIR/settings.json
+ln -s $"SCRIPT_ROOT/VSCode/settings.json" "$VSCODE_CFG_DIR/settings.json"
 
 rm -f ~/.config/Code/User/keybindings.json
-ln -s $SCRIPT_ROOT/VSCode/keybindings.json ~/.config/Code/User/keybindings.json
+ln -s "$SCRIPT_ROOT/VSCode/keybindings.json" ~/.config/Code/User/keybindings.json
 
 # Helix configs
 HELIX_CFG_DIR=~/.config/helix
 mkdir --parents $HELIX_CFG_DIR
 rm -f $HELIX_CFG_DIR/config.toml
-ln -s $SCRIPT_ROOT/helix/config.toml $HELIX_CFG_DIR/config.toml
+ln -s "$SCRIPT_ROOT/helix/config.toml" "$HELIX_CFG_DIR/config.toml"
 
 rm -f $HELIX_CFG_DIR/languages.toml
-ln -s $SCRIPT_ROOT/helix/languages.toml $HELIX_CFG_DIR/languages.toml
+ln -s "$SCRIPT_ROOT/helix/languages.toml" "$HELIX_CFG_DIR/languages.toml"
+
+HELIX_THEME_DIR=$HELIX_CFG_DIR/themes
+mkdir --parents $HELIX_THEME_DIR
+rm -f "$HELIX_THEME_DIR/alagyn.toml"
+ln -s "$SCRIPT_ROOT/helix/alagyn-theme.toml" "$HELIX_THEME_DIR/alagyn.toml"
